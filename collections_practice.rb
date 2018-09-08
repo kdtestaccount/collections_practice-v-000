6d247ff769666b9ba3993a996173ac6e0079fc38
kvdoherty@gmail.com <kvdoherty@gmail.com>
2018-09-08 19:37:57 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -11,8 +11,7 @@ def sort_array_char_count(array)
 end 
 
 def swap_elements(array)
-  array2 = array[1]
-  array[2] = array2
+  array[1], array[2] = array[2], array[1]
 end
 
 def reverse_array(array)
